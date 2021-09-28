f62a99e620b195b57bb9261dd0fc3c1476161f70
Matheus Mendes <=>
2021-09-27 22:16:42 -0300

    Criação de tela para cadastro de clientes

---
+++ src/main/java/br/com/mmsoftwares/easystock/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package br.com.mmsoftwares.easystock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @GetMapping("/cadastro")
+    public String cadastrar(){
+        return "/cliente/cadastro";
+    }
+}
